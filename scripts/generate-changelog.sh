f455343dda3c8141fc9e5533f38b67e7de3059d4
Daniele Campogiani <daniele.campogiani@gmail.com>
2015-11-09 12:47:54 +0100

    Travis: generate changelog from github changes

---
+++ scripts/generate-changelog.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+REPO_SLUG = "$TRAVIS_REPO_SLUG"
+IFS='/' read -a repoArray <<< "$REPO_SLUG"
+REPO_OWNER="${repoArray[0]}"
+REPO_NAME="${repoArray[1]}"
+
+github-changes -o $REPO_OWNER -r $REPO_NAME -b $TRAVIS_BRANCH -a -k ${gitHubAccessToken}
