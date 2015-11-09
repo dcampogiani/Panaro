a12468fe48487fcb75364be0e03a606a87d710a1
Daniele Campogiani <daniele.campogiani@gmail.com>
2015-11-09 15:19:21 +0100

    Travis: test upload to Fabric Beta on tags

--- scripts/upload-to-fabric.sh
+++ scripts/upload-to-fabric.sh
@@ -3,5 +3,6 @@ if [ -z ${TRAVIS_TAG+x} ]; then
   echo "Skipping upload to Fabric Beta since no tag was found";
 else
   echo "Uploading artifact to Fabric Beta";
+  echo $TRAVIS_TAG;
   ./gradlew assembleRelease crashlyticsUploadDistributionRelease;
 fi
